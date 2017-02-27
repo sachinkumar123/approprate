5f5f9380952cc211a50a994a666f86c9d81fbbff
abhishek singh <abhishek.s14@iiits.in>
2017-02-27 19:10:55 +0530

    :wip: Add REST App and settings in settings.py

--- cloudApp/cloudApp/settings.py
+++ cloudApp/cloudApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from REST_global_settings import REST_FRAMEWORK
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'interface',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
